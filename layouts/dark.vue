a465b220ffe6be40b8540c853eb05a5a626cabe1
mhafet17 <do.crazy192@gmail.com>
2020-05-28 07:37:25 +0700

    add layouts dark & error

---
+++ layouts/dark.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-black text-blue-900 p-64">
+    <h1 class="text-white uppercase text-center">Dark Mode</h1>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
